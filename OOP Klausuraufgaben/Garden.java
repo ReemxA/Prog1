3bcd4f3e65e52096791296ba3e42723c13bd24ad
ReemxA <reemabdelmagid@outlook.com>
2025-09-29 00:57:38 +0200

    Klasse Garden

---
+++ OOP Klausuraufgaben/Garden.java
@@ -0,0 +1,41 @@
+public class Garden {
+
+    /*
+     * Aufgabenstellung:
+     * 
+     * Erstellen Sie eine Klasse Garden.
+     * Ein Garten (Garden) hat folgende Eigenschaften:
+     * 
+     * 1. einen Namen (name)
+     * 2. ein Array für die Pflanzen im Garten (plants), 
+     *    dessen maximale Größe beim Erstellen des Gartens festgelegt wird
+     * 3. eine Variable currentSize, die die Anzahl der derzeit im Garten 
+     *    befindlichen Pflanzen speichert und mit 0 initialisiert wird
+     * 
+     * Erstellen Sie einen Konstruktor, der den Namen des Gartens 
+     * und die maximale Anzahl an Pflanzen im Array setzt.
+     * 
+     * Legen Sie in der Klasse Garden folgende Methoden an:
+     * 
+     * 1. addPlant(Plant plant): 
+     * Fügt eine neue Pflanze zum Garten hinzu, wenn noch Platz ist. 
+     * Falls das Array voll ist, gibt die Methode die Nachricht 
+     * "Keine weiteren Pflanzen können hinzugefügt werden" aus.
+     * 
+     * 2. waterAllPlants(int amount): 
+     * Gießt alle Pflanzen im Garten mit der angegebenen Wassermenge.
+     * 
+     * 3. listHealthyPlants(): 
+     * Gibt alle Pflanzen aus, die gesund sind 
+     * (verwenden Sie die Methode isHealthy() der Klasse Plant).
+     * 
+     * 4. findPlantByName(String name): 
+     * Gibt die Pflanze mit dem angegebenen Namen zurück 
+     * oder null, wenn keine solche Pflanze gefunden wurde.
+     */
+
+     // Attribute:
+     private String name;
+     private String[] plants;
+     private
+}
